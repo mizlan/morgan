d4eb119919534991123977e17dcbd896aef305f1
Michael Lan <michaellan202@gmail.com>
2021-04-09 10:04:34 -0700

    Add update script

---
+++ updater.clj
@@ -0,0 +1,38 @@
+#!/usr/bin/env bb
+
+(require '[babashka.fs :as fs]
+         '[bencode.core :as b])
+
+(def *port* 55901)
+
+(def base (slurp "base.md"))
+(def gh-repo-path "/Users/michaellan/code/mizlan/")
+(def output-path (str gh-repo-path "/README.md"))
+
+;; adapted from https://book.babashka.org/#_interacting_with_an_nrepl_server
+(defn nrepl-eval [port expr ns]
+  (let [s (java.net.Socket. "localhost" port)
+        out (.getOutputStream s)
+        in (java.io.PushbackInputStream. (.getInputStream s))
+        _ (b/write-bencode out {"op" "eval" "code" expr "ns" ns})
+        stuff (b/read-bencode in)
+        bytes (get stuff "value")]
+    (String. bytes)))
+
+(defn morgan-eval [expr]
+  (nrepl-eval *port* expr "morgan.main"))
+
+
+(let [ranks (morgan-eval "(map format-entry (get-most-recent conn 5))")]
+  (spit output-path
+        (str
+         base
+         "\n"
+         "| program | elapsed | finished |\n"
+         "| :---    | :---    | :---     |\n"
+         (str/join "\n" (read-string ranks)))))
+
+(shell/with-sh-dir gh-repo-path
+  (shell/sh "bash" "-c" "git add README.md")
+  (shell/sh "bash" "-c" "git commit -m'update'")
+  (shell/sh "bash" "-c" "git push"))
