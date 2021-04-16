633feaca97edfca403ea1c6fa0a7439f2181e111
Michael Lan <michaellan202@gmail.com>
2021-04-16 09:32:37 -0700

    Installer: override existing files

--- scripts/installer.clj
+++ scripts/installer.clj
@@ -1,6 +1,8 @@
 #!/usr/bin/env bb
 
+;; TODO: write an automation script for the daemon
 (require '[babashka.fs :as fs])
 
-(fs/copy "./man_shim.clj" "/usr/local/bin/m")
-(fs/copy "./updater.clj" "/usr/local/bin/morgan-update")
+(let [opts {:replace-existing true}]
+  (fs/copy "./man_shim.clj" "/usr/local/bin/m" opts)
+  (fs/copy "./updater.clj" "/usr/local/bin/morgan-update" opts))
